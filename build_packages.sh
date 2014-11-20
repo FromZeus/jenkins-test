9206fa76638cb45effaf64ba00168a1f2cf1634f
dtrishkin <dtrishkin@mirantis.com>
2014-11-20 18:40:12 +0300

    Test script

--- build_packages.sh
+++ build_packages.sh
@@ -1,19 +1,49 @@
-get_last_commits()
+build_packs()
 {
-	for i in "$(git log -n 2 --pretty=format:"%h")"
+	for i in $packs
 		do
-			echo $i
+			local depends
+			local dep
+			wget "$PURL"
+			mkdir $TARGZ
+			cd $TARGZ
+			dh_make -e "$EMAIL" -s -y -f "../$TARGZ"
+
+			depends=$(apt-get -s build-dep "$PNAME")
+			depends=${depends#*installed:}
+			depends=${depends%%upgraded*}
+			sudo apt-get build-dep "$PNAME" -y
+
+			for k in $depends
+				do
+					dependences+=("$k")
+				done
+			dependences=(${dependences[@]::${#dependences[@]} - 1})
+
+			for k in ${dependences[@]}
+				do
+					dep+="$k,"
+				done
+
+			cat "debian/control" | while IFS='' read -a line
+				do
+					if [[ ${line} == *Build-Depends:* ]]
+						then
+							echo "Build-Depends: $dep" >> "debian/control.buf"
+						else
+							echo "$line" >> "debian/control.buf"
+					fi
+				done
+
+			cat "debian/control.buf" > "debian/control"
+			rm -f "debian/control.buf"
+			DEB_BUILD_OPTIONS=nocheck dpkg-buildpackage -rfakeroot -uc -us -tc
+			cd ..
+			echo "Cleaning..."
+			sudo apt-get autoremove $(apt-cache showsrc $PNAME | sed -e '/Build-Depends/!d;s/Build-Depends: \|,\|([^)]*),*\|\[[^]]*\]//g')
+			find . -type f -not -name '*.sh' | xargs rm
+					
 		done
 }
 
-get_diff_files()
-{
-	git diff --name-only $(get_last_commits) *.tar.gz
-}
-
-list_pack=$(get_diff_files)
-
-for i in $list_pack
-	do
-		mkdir "${i%.tar.gz}"
-	done
+build_packs
--- get_commits_diff.sh
+++
@@ -1,9 +0,0 @@
-get_last_commits()
-{
-	for i in "$(git log -n 2 --pretty=format:"%h")"
-		do
-			echo $i
-		done
-}
-
-git diff --name-only $(get_last_commits) *.tar.gz
